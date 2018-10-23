945d6f97579f2d563632096b9788c5d79029cdc8
Brian Burns <alex.dzwonchyk@gmail.com>
2018-10-23 16:25:39 -0400

    Add filters for site data

--- client/src/App.js
+++ client/src/App.js
@@ -12,17 +12,40 @@ class App extends Component {
     super(props);
 
     this.state = {
+      searchTerm: '',
       codebaseSiteData: {
         actively_reporting_sites: [],
         not_actively_reporting_sites: [],
       },
       WPEngineInstalls: [],
+      filteredCodebaseSites: {
+        actively_reporting_sites: [],
+        not_actively_reporting_sites: [],
+      },
     }
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(e) {
+    const searchTerm = e.target.value;
+
+    // This line is the problem...we need to have state somewhere else in order to be retrieving it!
+    const allSites = this.state.codebaseSiteData.actively_reporting_sites;
+    const filteredSites = allSites.filter(site => site.url.includes(searchTerm));
+
+    this.setState(previousState => ({
+      searchTerm: searchTerm,
+      filteredCodebaseSites: {
+        actively_reporting_sites: filteredSites,
+        not_actively_reporting_sites: previousState.codebaseSiteData.not_actively_reporting_sites,
+      }
+    }));
   }
 
   componentDidMount() {
     callApi('/api/wpengine')
-    .then(res => this.setState({ WPEngineInstalls: res.results}))
+    .then(res => this.setState({WPEngineInstalls: res.results}))
     .catch(err => console.log(err));
 
     callApi('/api/codebase')
@@ -30,18 +53,26 @@ class App extends Component {
       codebaseSiteData: {
         actively_reporting_sites: responseData.actively_reporting_sites,
         not_actively_reporting_sites: responseData.not_actively_reporting_sites,
-      }
+      },
+      filteredCodebaseSites: {
+        actively_reporting_sites: responseData.actively_reporting_sites,
+        not_actively_reporting_sites: responseData.not_actively_reporting_sites,
+      },
     }))
     .catch(error => console.log(error));
   }
 
 
   render() {
-    console.log(this.state);
+    console.log(this.state.filteredCodebaseSites)
     return (
       <div className="App">
         <Header />
         <SearchBar />
+
+        <form>
+          <input type="text" name="filter" id="filter" placeholder="Search" onChange={this.handleFilter} />
+        </form>
 
         <Tabs>
           <TabList>
@@ -52,7 +83,7 @@ class App extends Component {
         
           <TabPanel>
             <h2>Actively Reporting Sites</h2>
-            <CodebaseSiteList siteData={this.state.codebaseSiteData}/>
+            <CodebaseSiteList siteData={this.state.filteredCodebaseSites} />
           </TabPanel>
           <TabPanel>
             <h2>Non-code base sites - Ads Next Framework, etc.</h2>
--- client/src/components/CodebaseSiteList.js
+++ client/src/components/CodebaseSiteList.js
@@ -1,4 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+// class CodebaseSiteList extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       actively_reporting_sites: props.siteData.actively_reporting_sites,
+//     }
+
+//     this.handleFilter = this.handleFilter.bind(this);
+//   }
+
+//   handleFilter(e) {
+//     const searchTerm = e.target.value;
+//     const allSites = this.props.siteData.actively_reporting_sites;
+//     console.log(allSites);
+//     const filteredSites = allSites.filter(site => {site.url.includes(searchTerm)});
+
+//     this.setState({
+//       actively_reporting_sites: filteredSites,
+//     });
+//   }
+
+//   componentWillReceiveProps(nextProps) {
+//     this.setState({actively_reporting_sites: nextProps.siteData.actively_reporting_sites})
+//   }
+
+//   // componentDidMount(props) {
+//   //   this.state = {
+//   //     actively_reporting_sites: props.siteData.actively_reporting_sites,
+//   //   }
+//   // }
+
+//   render() {
+//     return (
+//       <div className="limiter">
+//       <input type="text" name="filter" id="filter" onChange={this.handleFilter} />
+//         <table cellSpacing="0">
+//           <thead>
+//               <tr>
+//                 <th>URL</th>
+//                 <th>Codebase Version</th>
+//                 <th>Child Theme</th>
+//                 <th>Child Theme Version</th>
+//                 <th>Last Checked</th>
+//               </tr>
+//           </thead>
+//           <tbody>
+//           {this.state.actively_reporting_sites.map((site) => {
+//           return (
+//             <tr key={site.id}>
+//               <td>{site.url}</td>
+//               <td>{site.cb_v}</td>
+//               <td>{site.ds_name}</td>
+//               <td>{site.ds_v}</td>
+//               <td>{site.last_updated}</td>
+//             </tr>
+//           )})}
+//           </tbody>
+//         </table>
+//       </div>
+//     );
+//   }
+// }
 
 const CodebaseSiteList = (props) => {
   return (
@@ -27,7 +91,7 @@ const CodebaseSiteList = (props) => {
         </tbody>
       </table>
       
-      <h2>Not Actively Reporting Sites</h2>
+      {/* <h2>Not Actively Reporting Sites</h2>
 
       <table cellSpacing="0">
       <thead>
@@ -51,7 +115,7 @@ const CodebaseSiteList = (props) => {
         </tr>
       )})}
       </tbody>
-      </table>
+      </table> */}
     </div>
   );
 }
